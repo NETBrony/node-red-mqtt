0828bd99de2595ced48088a7e282777482f25b61
NETBrony <natchanon3343@hotmail.com>
2025-12-20 20:45:28 +0700

    edit ssid pass

--- src/secret.h
+++ src/secret.h
@@ -1,10 +1,10 @@
 #ifndef SECRET_H
 #define SECRET_H
 
-char ssid[] = "@ Capsule Office";
-char pass[] = "pJoeInwOnlyFan";
-// char ssid[] = "NETBrony_2.4G";
-// char pass[] = "Net_3343254622";
+// char ssid[] = "@ Capsule Office";
+// char pass[] = "pJoeInwOnlyFan";
+char ssid[] = "NETBrony_2.4G";
+char pass[] = "Net_3343254622";
 
 const char* mqtt_server = "e53e2233d1a141699f1204a648c861c2.s1.eu.hivemq.cloud";
 const int   mqtt_port   = 8883;
